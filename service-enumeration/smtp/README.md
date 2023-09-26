9bed295e66ba1d76596de47243a90d7afe3df10c
0xA <43623870+zylideum@users.noreply.github.com>
2023-09-25 20:44:29 -0700

    Added SMTP Enumeration

---
+++ service-enumeration/smtp/README.md
@@ -0,0 +1,50 @@
+# Documentation
+
+Documentation for all tools in this section are available below.
+
+# smtpusers
+
+`python3 <IP> </wordlist/path> [--output FILE] [--batch-size INT]`
+
+#### Required Arguments
+- `<IP>` like 192.168.0.1
+- `</wordlist/path>` like /usr/share/wordlists/etc
+
+#### Optional Arguments
+- *`--output`* outputs valid usernames to a file.
+- *`--batch-size`* controls the amount of commands sent per connection.
+
+#### Sample Usage
+
+>`python3 smtpusers.py 192.168.0.1 /wordlist/file --output users.txt --batch-size 100`
+
+#### How it Works
+
+The script initiates a connection to SMTP port 25 on the target and issued a `VRFY` command for each user in the wordlist. If the user exists, the target will respond with a code of 252.
+
+The *`batch-size`* argument was created due to issues with rate limiting and disconnects from SMTP targets. Each batch is the number of `VRFY` command ran in one connection to SMTP. By default, this value is 10; however, it can be increased as high as the target server will support.
+
+#### Warning
+
+This script can be relatively slow as far as brute-forcing goes. Increase the *`batch-size`* where possible and use smaller wordlist.
+
+#### Installation
+
+```bash
+wget https://raw.githubusercontent.com/zylideum/hacktory-aux/main/service-enumeration/smtp/smtpusers.py;
+```
+
+#### Screenshots
+
+<p align="center">
+  <img src="/service-enumeration/smtp/readme-imgs/smtpusers.png" />
+</p>
+<p align="center"><i>Fig. 1 Using a lengthy wordlist against an SMTP server.</i></p>
+<br>
+<br>
+<p align="center">
+  <img src="/service-enumeration/smtp/readme-imgs/smtpresults.png" />
+</p>
+<p align="center"><i>Fig. 4 Output of scan when using --output flag.</i></p>
+<br>
+<br>
---
+++ service-enumeration/smtp/smtpusers.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+
+import socket
+import sys
+import argparse
+from termcolor import colored
+
+def create_empty_file(output_file):
+    with open(output_file, 'w') as output:
+        pass  # Creates an empty file or overwrites if it already exists
+
+def enumerate_users(ip, wordlist, output_file=None, batch_size=10):
+    batch_count = 0
+    s = None  # Initialize the socket outside the loop
+
+    if output_file:
+        create_empty_file(output_file)
+
+    try:
+        with open(wordlist, 'r') as file:
+            total_users = len(file.readlines())
+            file.seek(0)  # Reset file position to the beginning
+            users_found = []
+
+            for idx, line in enumerate(file):
+                if batch_count == 0:
+                    if s:
+                        s.close()  # Close the previous connection if it exists
+                    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    connect = s.connect((ip, 25))
+                    banner = s.recv(1024)
+                    batch_count = batch_size
+
+                user = line.strip().encode()
+                s.send(b'VRFY ' + user + b'\r\n')
+                result = s.recv(1024)
+
+                if b'252' in result:
+                    found_user = user.decode()
+                    users_found.append(found_user)
+                    print(colored(f" User {found_user} found!", 'green'))
+                    if output_file:
+                        with open(output_file, 'a') as output:
+                            output.write(found_user + '\n')
+
+                progress_message = f"Checking users... [{idx + 1}/{total_users}]"
+                sys.stdout.write('\r' + progress_message)
+                sys.stdout.flush()
+                batch_count -= 1
+
+            sys.stdout.write('\n')  # Print a newline to clear the progress message
+
+        print("Enumeration complete.")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        if s:
+            s.close()  # Close the connection if it's still open
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='SMTP User Enumeration Script')
+    parser.add_argument('ip', help='SMTP server IP address')
+    parser.add_argument('wordlist', help='Wordlist file containing usernames')
+    parser.add_argument('--output', help='Optional output file to store valid usernames')
+    parser.add_argument('--batch-size', type=int, default=10, help='Batch size for opening connections')
+
+    args = parser.parse_args()
+
+    enumerate_users(args.ip, args.wordlist, args.output, args.batch_size)
